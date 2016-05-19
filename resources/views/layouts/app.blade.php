4d87fccc77c48d2dcee0c7093a7e3fc8be8dff96
jambik <jambik@gmail.com>
2016-05-19 20:51:05 +0300

    - first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -132,7 +132,7 @@
 
     <section id="map">
         <div class="container-fluid">
-
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=18BTv-YBIoyQpnrPBQS7rhPH6fuHf7Pd&width=100%&height=720&lang=ru_RU&sourceType=constructor&scroll=true"></script>
         </div>
     </section>
 </div>
